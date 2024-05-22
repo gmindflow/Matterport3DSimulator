6518a9d7b0d1e334552f358a5fa6a9030e224181
gmindflow <gmindflow@gmail.com>
2024-05-22 04:54:04 +0000

    minor change in run_docker1.sh code

--- scripts/run_docker1.sh
+++ scripts/run_docker1.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Run docker container
-nvidia-docker run -it --mount type=bind,source=$MATTERPORT_DATA_DIR,target=/root/mount/Matterport3DSimulator/data/v1/scans --volume `pwd`:/root/mount/Matterport3DSimulator mattersim:9.2-devel-ubuntu18.04
+sudo nvidia-docker run -it --mount type=bind,source=$MATTERPORT_DATA_DIR,target=/root/mount/Matterport3DSimulator/data/v1/scans --volume `pwd`:/root/mount/Matterport3DSimulator mattersim:9.2-devel-ubuntu18.04
+
+sudo nvidia-docker run -it \
+  --mount type=bind,source=$MATTERPORT_DATA_DIR,target=/root/mount/Matterport3DSimulator/data/v1/scans \
+  --mount type=bind,source=/home/jihun/workspace/repositories/commonsense/mqa/NaviLLM,target=/root/mount/NavIllm \
+  --volume `pwd`:/root/mount/Matterport3DSimulator \
+  mattersim:9.2-devel-ubuntu18.04
 
 # Inside docker container: below codes do not run.
 # Build simulator code
